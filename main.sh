626d0a52b288cca1938a566f011471fa2ba21fd1
meowmax <maximillian@disroot.org>
2024-03-05 21:18:45 +1300

    fix: unzip prompting for user input

--- main.sh
+++ main.sh
@@ -9,16 +9,16 @@ export FTCVER="0.19.2.110"
 
 wget https://github.com/MarmadileManteater/FreeTubeCordova/releases/download/${FTCVER}/freetube-${FTCVER}-pwa.zip 
 
-unzip freetube-${FTCVER}-pwa -d freetube-pwa
+unzip freetube-${FTCVER}-pwa -o -d freetube-pwa
 
-#git clone https://github.com/gamingdoom/neutron --recurse-submodules -j8
-#cp config.json neutron/build/config.json
-#cd neutron/
-#python -m venv venv
-#source venv/bin/activate
-#pip install -r requirements.txt
-#python configurator.py -c build/config.json 
-#cd build
-#python build.py
+git clone https://github.com/gamingdoom/neutron --recurse-submodules -j8
+cp config.json neutron/build/config.json
+cd neutron/
+python -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+python configurator.py -c build/config.json 
+cd build
+python build.py
 
 #bundle exe files with wrapper script that runs localserver command and nuetron build simultainously
