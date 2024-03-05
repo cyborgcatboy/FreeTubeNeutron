a74612a73c8152668128beff1fca9418f155e990
meowmax <maximillian@disroot.org>
2024-03-05 21:17:05 +1300

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# FreeTubeNeutron
+
+A freetube packaging that uses [FreeTubeCordova's](https://github.com/MarmadileManteater/FreeTubeCordova) progressive web app, along with the firefox-based [Neutron](https://github.com/gamingdoom/neutron) web environment wrapper.
+
+## Dependancies
+
+the specific depenancies for this script are:
+
+ - git 
+ - unzip 
+ - wget 
+ - python
+
+## Building
+
+Run:
+
+```shell
+./main.sh
+```
---
+++ main.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# freetube pwa from freetubeCordova
+# have nuetron config.json
+# build neutron
+# host pwa on localhost server
+# build neutron for localhost server 
+
+export FTCVER="0.19.2.110"
+
+wget https://github.com/MarmadileManteater/FreeTubeCordova/releases/download/${FTCVER}/freetube-${FTCVER}-pwa.zip 
+
+unzip freetube-${FTCVER}-pwa -d freetube-pwa
+
+#git clone https://github.com/gamingdoom/neutron --recurse-submodules -j8
+#cp config.json neutron/build/config.json
+#cd neutron/
+#python -m venv venv
+#source venv/bin/activate
+#pip install -r requirements.txt
+#python configurator.py -c build/config.json 
+#cd build
+#python build.py
+
+#bundle exe files with wrapper script that runs localserver command and nuetron build simultainously
