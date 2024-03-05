cf012233f4e01fc859ef3283a7760e9fab668ac0
meowmax <maximillian@disroot.org>
2024-03-05 23:33:33 +1300

    feat: caddy now works

--- main.sh
+++ main.sh
@@ -4,6 +4,7 @@
 # build neutron
 # host pwa on localhost server
 # build neutron for localhost server 
+export SDIR="$(pwd)"
 
 export FTCVER="0.19.2.110"
 export CDYVER="2.7.6"
@@ -22,19 +23,20 @@ cd neutron/
 python -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
-python configurator.py -c config.json 
+#python configurator.py -c config.json 
 cd build
-python build.py
+#python build.py
 
-#cd ../..
-#if command -v caddy &> /dev/null; then
-#  # use system caddy to run http server
-#  echo "caddy found! using..."
-#else
-#  echo "no caddy!"
-#  wget "https://github.com/caddyserver/caddy/releases/download/v${CDYVER}/caddy_${CDYVER}_${CDYARC}.tar.gz" -O caddy.tar.gz;
-#  tar -zxf caddy.tar.gz;
-#  ./caddy file-server -r freetube-pwa/ -l :21999  
-#fi
+cd ${SDIR}/freetube-pwa
+if command -v caddy &> /dev/null; then
+  # use system caddy to run http server
+  echo "caddy found! using..."
+  caddy file-server -r . -l :21999
+else
+  echo "no caddy!"
+  wget "https://github.com/caddyserver/caddy/releases/download/v${CDYVER}/caddy_${CDYVER}_${CDYARC}.tar.gz" -O caddy.tar.gz;
+  tar -zxf caddy.tar.gz caddy; 
+  ./caddy file-server -r . -l :21999  
+fi
 
 #bundle exe files with wrapper script that runs localserver command and nuetron build simultainously
