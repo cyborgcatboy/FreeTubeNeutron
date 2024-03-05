8b499b76f5253fafe9ffde03a5db0d52f4338737
meowmax <maximillian@disroot.org>
2024-03-05 23:21:04 +1300

    feat: start on adding http server

--- main.sh
+++ main.sh
@@ -6,10 +6,13 @@
 # build neutron for localhost server 
 
 export FTCVER="0.19.2.110"
+export CDYVER="2.7.6"
+export CDYARC="linux_amd64"
 
-wget https://github.com/MarmadileManteater/FreeTubeCordova/releases/download/${FTCVER}/freetube-${FTCVER}-pwa.zip 
+wget "https://github.com/MarmadileManteater/FreeTubeCordova/releases/download/${FTCVER}/freetube-${FTCVER}-pwa.zip" -O freetube-pwa.zip
 
-unzip freetube-${FTCVER}-pwa -o -d freetube-pwa
+rm -r freetube-pwa
+unzip freetube-pwa.zip -d freetube-pwa
 
 git clone https://github.com/gamingdoom/neutron --recurse-submodules -j8
 
@@ -22,5 +25,16 @@ pip install -r requirements.txt
 python configurator.py -c config.json 
 cd build
 python build.py
+
+#cd ../..
+#if command -v caddy &> /dev/null; then
+#  # use system caddy to run http server
+#  echo "caddy found! using..."
+#else
+#  echo "no caddy!"
+#  wget "https://github.com/caddyserver/caddy/releases/download/v${CDYVER}/caddy_${CDYVER}_${CDYARC}.tar.gz" -O caddy.tar.gz;
+#  tar -zxf caddy.tar.gz;
+#  ./caddy file-server -r freetube-pwa/ -l :21999  
+#fi
 
 #bundle exe files with wrapper script that runs localserver command and nuetron build simultainously
